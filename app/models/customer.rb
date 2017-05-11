a32fb124a1658730a0852e63dc2bfc68664dbed8
Andy Primawan <andy@primawan.com>
2017-05-12 06:40:41 +0700

    add 3 main models

---
+++ app/models/customer.rb
@@ -0,0 +1,10 @@
+class Customer
+  include Cequel::Record
+
+  key :id, :uuid
+  column :name, :text
+  column :key_id, :text
+  column :key_secret, :text
+  column :is_active, :boolean
+  timestamps
+end
