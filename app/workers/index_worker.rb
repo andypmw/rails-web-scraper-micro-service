c638208b89efb616a6c6ac8382ed3b18f60cc377
Andy Primawan <andy@primawan.com>
2017-05-12 22:51:51 +0700

    Index Worker draft

--- app/workers/index_worker.rb
+++ app/workers/index_worker.rb
@@ -1,7 +1,30 @@
+require 'capybara/rails'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
+
 class IndexWorker
   include Sidekiq::Worker
+  include Capybara::DSL
 
   def perform(*args)
-    # Do something
+    Capybara.javascript_driver = :poltergeist
+
+    visit args['page_url']
+
+    page.all('h1').each do |element|
+      Index_content.new(index_job_id: args['index_job_id'], type: 'h1', content: element.text).save!
+    end
+
+    page.all('h2').each do |element|
+      Index_content.new(index_job_id: args['index_job_id'], type: 'h2', content: element.text).save!
+    end
+
+    page.all('h3').each do |element|
+      Index_content.new(index_job_id: args['index_job_id'], type: 'h3', content: element.text).save!
+    end
+
+    page.all('a').each do |element|
+      Index_content.new(index_job_id: args['index_job_id'], type: 'anchor', content: element['href']).save!
+    end
   end
 end
