b747d0b19f2b6c5fcbf2ee5d04057d556aa2495b
Andy Primawan <andy@primawan.com>
2017-05-13 11:21:29 +0700

    write test for create new index job endpoint

--- test/controllers/index_job_controller_test.rb
+++ test/controllers/index_job_controller_test.rb
@@ -11,9 +11,32 @@ class IndexJobControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get create" do
-    get create_path
-    assert_response :success
+  test "index job shouldn't created if customer_id or page_url is empty" do
+    post create_path, params: {
+      customer_id: '2e9d39ca-b708-4870-bbff-c21f55cadc7f'
+    }
+    assert_response 400
+
+    post create_path, params: {
+      page_url: 'http://andy.primawan.com'
+    }
+    assert_response 400
+  end
+
+  test "index job shouldn't created if page_url is not a valid HTTP or HTTPS URI" do
+    post create_path, params: {
+      customer_id: '2e9d39ca-b708-4870-bbff-c21f55cadc7f',
+      page_url: 'ftp://andy.primawan.com'
+    }
+    assert_response 400
+  end
+
+  test "index job should created if parameters is complete" do
+    post create_path, params: {
+      customer_id: '2e9d39ca-b708-4870-bbff-c21f55cadc7f',
+      page_url: 'http://andy.primawan.com'
+    }
+    assert_response 201
   end
 
   test "should get cancel" do
