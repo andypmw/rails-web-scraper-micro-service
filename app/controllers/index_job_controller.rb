f95c367852dd3cf90f0677d6c56368f23b2ba509
Andy Primawan <andy@primawan.com>
2017-05-13 11:20:49 +0700

    add simple validation check at create new index job endpoint

--- app/controllers/index_job_controller.rb
+++ app/controllers/index_job_controller.rb
@@ -1,4 +1,5 @@
 require 'securerandom'
+require 'uri'
 
 class IndexJobController < ApplicationController
   def index
@@ -11,6 +12,22 @@ class IndexJobController < ApplicationController
     customer_id = params['customer_id']
     page_url = params['page_url']
     index_job_id = SecureRandom.uuid
+
+    if customer_id.nil? or page_url.nil?
+      render status: 400, json: { 
+        status: 400, 
+        error: 'Bad Request', 
+        message: 'You must supply customer_id and page_url' 
+      } and return
+    end
+
+    unless page_url =~ /\A#{URI::regexp(['http', 'https'])}\z/
+      render status: 400, json: { 
+        status: 400, 
+        error: 'Bad Request', 
+        message: 'You must supply valid HTTP or HTTPS page_url' 
+      } and return
+    end
 
     IndexJob.new(
       customer_id: customer_id,
