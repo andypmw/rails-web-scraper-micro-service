54c4faac8e300c98a7fba7ad3cecbb29ea650a21
Andy Primawan <andy@primawan.com>
2017-05-13 10:37:08 +0700

    Index Job worker using PhantomJS, with Chrome’s user-agent.

--- app/controllers/index_job_controller.rb
+++ app/controllers/index_job_controller.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 class IndexJobController < ApplicationController
   def index
   end
@@ -6,6 +8,20 @@ class IndexJobController < ApplicationController
   end
 
   def create
+    customer_id = params['customer_id']
+    page_url = params['page_url']
+    index_job_id = SecureRandom.uuid
+
+    IndexJob.new(
+      customer_id: customer_id,
+      id: index_job_id,
+      page_url: page_url,
+      status: :wait
+    ).save!
+    
+    IndexWorker.perform_async(customer_id, index_job_id)
+
+    render status: 201, json: { index_job_id: index_job_id }
   end
 
   def cancel
--- app/workers/index_worker.rb
+++ app/workers/index_worker.rb
@@ -1,30 +1,82 @@
-require 'capybara/rails'
+require 'capybara'
 require 'capybara/dsl'
 require 'capybara/poltergeist'
+require 'uri'
 
 class IndexWorker
   include Sidekiq::Worker
   include Capybara::DSL
 
-  def perform(*args)
-    Capybara.javascript_driver = :poltergeist
+  def perform(customer_id, index_job_id)
+    index_job = Customer.find(customer_id).index_jobs.find(index_job_id)
+    uri = URI(index_job.page_url)
 
-    visit args['page_url']
+    Capybara.current_driver = :poltergeist
+    Capybara.run_server = false
+    if uri.scheme.nil? then
+      Capybara.app_host = "http://#{uri.host}" 
+    else
+      Capybara.app_host = "#{uri.scheme}://#{uri.host}"
+    end
+
+    page.driver.headers = { "User-Agent" => "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36" }
+
+    visit "#{uri.path}#{uri.query}#{uri.fragment}"
+
+
+
+    content_order = 1
 
     page.all('h1').each do |element|
-      Index_content.new(index_job_id: args['index_job_id'], type: 'h1', content: element.text).save!
+      content_order += 1
+      IndexJobContent.new(
+        index_job_customer_id: customer_id, 
+        index_job_id: index_job_id,
+        content_order: content_order.next,
+        type: :h1, 
+        content: element.text
+      ).save!
     end
 
     page.all('h2').each do |element|
-      Index_content.new(index_job_id: args['index_job_id'], type: 'h2', content: element.text).save!
+      content_order += 1
+      IndexJobContent.new(
+        index_job_customer_id: customer_id, 
+        index_job_id: index_job_id,
+        content_order: content_order.next,
+        type: :h2, 
+        content: element.text
+      ).save!
     end
 
     page.all('h3').each do |element|
-      Index_content.new(index_job_id: args['index_job_id'], type: 'h3', content: element.text).save!
+      content_order += 1
+      IndexJobContent.new(
+        index_job_customer_id: customer_id, 
+        index_job_id: index_job_id,
+        content_order: content_order.next,
+        type: :h3, 
+        content: element.text
+      ).save!
     end
 
     page.all('a').each do |element|
-      Index_content.new(index_job_id: args['index_job_id'], type: 'anchor', content: element['href']).save!
+      content_order += 1
+      IndexJobContent.new(
+        index_job_customer_id: customer_id, 
+        index_job_id: index_job_id,
+        content_order: content_order.next,
+        type: :anchor, 
+        content: element['href']
+      ).save!
     end
+
+
+    if page.status_code == '200' then
+      index_job.status = :success
+    else
+      index_job.status = :fail
+    end
+    index_job.save!
   end
 end
